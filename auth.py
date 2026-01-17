0e908221303d8b6ded9435ebcfe6b15a47c499ea
alassauzet <alix.lassauzet@gmail.com>
2026-01-17 14:42:01 +0100

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
+from config import USERS_FILE
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+def get_user(username):
+    df = pd.read_csv(USERS_FILE)
+    row = df[df.username == username]
+    if row.empty:
+        return None
+    return {
+        "username": username,
+        "password_hash": row.iloc[0]["password_hash"]
+    }
+
+def authenticate(username, password):
+    user = get_user(username)
+    if user and check_password_hash(user["password_hash"], password):
+        return User(username)
+    return None
